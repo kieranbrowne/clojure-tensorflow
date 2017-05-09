ef0321b64bcda23e25bf2f376f31d82dc2e3aaeb
Kieran Browne <kierantbrowne@gmail.com>
2017-05-10 00:00:36 +1000

    Gradient calc seems to be working; need to clean code.

--- src/clojure_tensorflow/build.clj
+++ src/clojure_tensorflow/build.clj
@@ -3,7 +3,14 @@
 
 (def default-graph (new org.tensorflow.Graph))
 
+;; As a design choice from tensorflow graph needs to be
 (def global-variables (atom []))
+
+;; The shadow graph patches a couple of requirements while we wait for
+;; the Java api. We store all relevant information about operations as
+;; we add them to the graph. Eventually all of this will be extractable
+;; from the java graph / operations objects.
+(def shadow-graph (atom []))
 
 (defn op-builder
   "Returns a function which creates an operation for the graph"
@@ -12,6 +19,7 @@
    (let [{:keys [operation node-name attrs inputs]
           :or {node-name (str (gensym operation)) attrs {} inputs []}
           } op-profile]
+     (swap! shadow-graph conj (assoc op-profile :name node-name :attrs attrs :inputs inputs))
      (utils/thread graph
                    (flatten
                     [#(.opBuilder % operation node-name)
@@ -27,3 +35,4 @@
                                         )) inputs)
                      #(.build %)
                      #(.output % 0)])))))
+
--- test/clojure_tensorflow/core_test.clj
+++ test/clojure_tensorflow/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [clojure-tensorflow.core :refer :all]
             [clojure-tensorflow.ops :as tf]
+            [clojure-tensorflow.build :as build]
             ))
 
 (deftest test-session-running
@@ -9,3 +10,45 @@
     (is (= (session-run [(tf/constant [1])])
            [1])))
   )
+
+(deftest test-gradients
+  (let [var-two (tf/variable 2.)
+        three-pow-two (tf/pow (tf/constant 3.) var-two)]
+    (testing "Get gradient for Pow"
+      (is (= (session-run [(tf/global-variables-initializer)
+                           (tf/gradients three-pow-two var-two)])
+             6.)))
+    ))
+
+(def input (tf/constant [[1. 0. 1.]]))
+(def output (tf/constant [[0.5]]))
+
+(def weights (tf/variable (repeatedly 3 #(vector (dec (* 2 (rand)))))))
+
+(def model (tf/sigmoid (tf/matmul input weights)))
+
+(def cost (tf/sub output model))
+
+(session-run
+ [(tf/global-variables-initializer)
+  cost])
+
+(session-run
+ [(tf/global-variables-initializer)
+  (tf/gradients cost weights)])
+
+(session-run
+ [(tf/global-variables-initializer)
+  ;; (tf/assign weights (tf/sub weights (tf/transpose (tf/gradients cost weights))))
+  cost
+  ])
+
+(session-run
+ [(tf/global-variables-initializer)
+  (tf/numerical-gradients cost weights)
+  ])
+
+(session-run
+ [(tf/get-registered-gradient (tf/sigmoid (tf/constant 9.)))])
+
+()
