d0c0df5dd085940c1d86655c45f5a07ccd2b51f2
Kieran Browne <kierantbrowne@gmail.com>
2017-05-15 21:06:18 +1000

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/kieranbrowne/clojure-tensorflow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :lein-release {:deploy-via :clojars}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.tensorflow/tensorflow  "1.1.0-rc1"]
                  ])
