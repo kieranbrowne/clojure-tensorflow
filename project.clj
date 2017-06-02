00840afe1d2fa27fec28e242551f90875004e10e
Kieran Browne <kierantbrowne@gmail.com>
2017-06-02 13:45:18 +1000

    Version 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-tensorflow/clojure-tensorflow "0.1.5"
+(defproject clojure-tensorflow/clojure-tensorflow "0.1.6"
   :description "A very light layer over Java interop for working with TensorFlow"
   :url "http://github.com/kieranbrowne/clojure-tensorflow"
   :license {:name "Eclipse Public License"
